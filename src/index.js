940e2640ee55245924ac818c75635cbc18acc994
zhipenglin <sunandmoon001@163.com>
2024-11-18 13:41:40 +0800

    添加新的api导出

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export { default as FilePreview, HtmlPreview, PdfPreview, TextPreview, ImagePrev
 export { default as FileButton, useFileModalProps, useFileModal, FileModal } from './components/FileButton';
 export { default as Image } from './components/Image';
 export { default as FileList, OptionButtons as FileListOptionButtons } from './components/FileList';
-export { default as FileUpload, FileInput, useFileUpload, defaultAccept } from './components/FileUpload';
+export { default as FileUpload, FileInput, useFileUpload, defaultAccept, computedAccept } from './components/FileUpload';
+export { default as withOSSFile } from './hocs/withOSSFile';
